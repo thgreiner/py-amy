1444c799cca8ee70db5341c06d26c1cef3c8d32d
Thorsten Greiner <thorsten.greiner@zalando.de>
2018-10-23 16:15:07 +0200

    Implement simple EPD parsing.

--- py_amy/engine/board.py
+++ py_amy/engine/board.py
@@ -1,6 +1,52 @@
 from py_amy.engine.constants import *
 
-INITIAL_POSITON = 'RNBQKBNR/PPPPPPPP/8/8/8/8/pppppppp/rnbqkbnr w KQkq -'
+INITIAL_POSITON = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -'
+
+PIECE_BY_NAME = {
+    'p': Piece.PAWN,
+    'P': Piece.PAWN,
+    'n': Piece.KNIGHT,
+    'N': Piece.KNIGHT,
+    'b': Piece.BISHOP,
+    'B': Piece.BISHOP,
+    'r': Piece.ROOK,
+    'R': Piece.ROOK,
+    'q': Piece.QUEEN,
+    'Q': Piece.QUEEN,
+    'k': Piece.KING,
+    'K': Piece.KING,
+}
+
+COLOR_BY_NAME = {
+    'p': Color.BLACK,
+    'P': Color.WHITE,
+    'n': Color.BLACK,
+    'N': Color.WHITE,
+    'b': Color.BLACK,
+    'B': Color.WHITE,
+    'r': Color.BLACK,
+    'R': Color.WHITE,
+    'q': Color.BLACK,
+    'Q': Color.WHITE,
+    'k': Color.BLACK,
+    'K': Color.WHITE,
+}
+
+def piecesFor(c):
+    for s in c:
+        if s >= '1' and s <= '8':
+            for i in range(int(s)):
+                yield None
+        else:
+            yield PIECE_BY_NAME[s]
+
+def colorsFor(c):
+    for s in c:
+        if s >= '1' and s <= '8':
+            for i in range(int(s)):
+                yield None
+        else:
+            yield COLOR_BY_NAME[s]
 
 class Board:
     """
@@ -8,15 +54,11 @@ class Board:
     """
 
     def __init__(self, epd = INITIAL_POSITON):
-        self.pieces = [
-            Piece.KING, None, Piece.KING, None, None, None, None, None,
-            Piece.PAWN
-        ]
-        self.colors = [
-            Color.WHITE, None, Color.BLACK, None, None, None, None, None,
-            Color.WHITE
-        ]
-        pass
+        t = epd.split(' ')
+        pcs = t[0].split('/')
+        pcs.reverse()
+        self.pieces = list(piecesFor("".join(pcs)))
+        self.colors = list(colorsFor("".join(pcs)))
 
     def get(self, idx):
         return (self.pieces[idx], self.colors[idx])
--- tests/engine/test_board.py
+++ tests/engine/test_board.py
@@ -6,12 +6,52 @@ from py_amy.engine.constants import *
 class BoardTests(unittest.TestCase):
     def testInitialPosition(self):
         board = Board()
-        piece, color = board.get(8)
-        self.assertEqual(Piece.PAWN, piece)
-        self.assertEqual(Color.WHITE, color)
+        for wp in range(8,16):
+            piece, color = board.get(wp)
+            self.assertEqual(Piece.PAWN, piece)
+            self.assertEqual(Color.WHITE, color)
+        for bp in range(48,56):
+            piece, color = board.get(bp)
+            self.assertEqual(Piece.PAWN, piece)
+            self.assertEqual(Color.BLACK, color)
+        for empty in range(16, 48):
+            piece, color = board.get(empty)
+            self.assertEqual(None, piece)
+            self.assertEqual(None, color)
+        for i in range(0, 8):
+            piece, color = board.get(i)
+            self.assertEqual(Color.WHITE, color)
+            piece, color = board.get(56+i)
+            self.assertEqual(Color.BLACK, color)
+        for i in [0, 7]:
+            piece, color = board.get(i)
+            self.assertEqual(Piece.ROOK, piece)
+            piece, color = board.get(56+i)
+            self.assertEqual(Piece.ROOK, piece)
+        for i in [1, 6]:
+            piece, color = board.get(i)
+            self.assertEqual(Piece.KNIGHT, piece)
+            piece, color = board.get(56+i)
+            self.assertEqual(Piece.KNIGHT, piece)
+        for i in [2, 5]:
+            piece, color = board.get(i)
+            self.assertEqual(Piece.BISHOP, piece)
+            piece, color = board.get(56+i)
+            self.assertEqual(Piece.BISHOP, piece)
+        for i in [3]:
+            piece, color = board.get(i)
+            self.assertEqual(Piece.QUEEN, piece)
+            piece, color = board.get(56+i)
+            self.assertEqual(Piece.QUEEN, piece)
+        for i in [4]:
+            piece, color = board.get(i)
+            self.assertEqual(Piece.KING, piece)
+            piece, color = board.get(56+i)
+            self.assertEqual(Piece.KING, piece)
+
 
     def testFromEPD(self):
-        board = Board("K1k5/8/8/8/8/8/8/8/8 w - -")
+        board = Board("8/8/8/8/8/8/8/8/K1k5 w - -")
         piece, color = board.get(0)
         self.assertEqual(Piece.KING, piece)
         self.assertEqual(Color.WHITE, color)
