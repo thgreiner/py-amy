113765a8749e93e40a4d214d523a4f528dfa984d
Thorsten Greiner <thorsten.greiner@zalando.de>
2018-10-23 14:32:53 +0200

    Create a simple board class and unit test.

---
+++ tests/engine/test_board.py
@@ -0,0 +1,10 @@
+import unittest
+
+from py_amy.engine.board import Board
+
+class BoardTests(unittest.TestCase):
+    def testInitialPosition(self):
+        board = Board()
+
+if __name__ == '__main__':
+    unittest.main()
