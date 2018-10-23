11b09730bca1e3673f60c8247b376dbcc3e522d8
Thorsten Greiner <thorsten.greiner@zalando.de>
2018-10-23 15:14:09 +0200

    Add enums for pieces and colors, add get() method to Board.

--- py_amy/engine/board.py
+++ py_amy/engine/board.py
@@ -1,3 +1,8 @@
+from py_amy.engine.constants import *
+
 class Board:
     def __init__(self):
-        print("New Board created.")
+        pass
+
+    def get(self, idx):
+        return (Piece.PAWN, Color.WHITE)
--- tests/engine/test_board.py
+++ tests/engine/test_board.py
@@ -1,10 +1,14 @@
 import unittest
 
 from py_amy.engine.board import Board
+from py_amy.engine.constants import *
 
 class BoardTests(unittest.TestCase):
     def testInitialPosition(self):
         board = Board()
+        piece, color = board.get(8)
+        self.assertEqual(Piece.PAWN, piece)
+        self.assertEqual(Color.WHITE, color)
 
 if __name__ == '__main__':
     unittest.main()
