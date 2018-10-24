e0c9184b8bc6c9215d549dfac813ca9541de9b8c
Thorsten Greiner <thorsten.greiner@zalando.de>
2018-10-24 15:18:54 +0200

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+
+from setuptools import setup, find_packages
+
+package_path = os.path.join('.')
+
+setup(
+    name='py_amy',
+    version='0.1',
+    author='Thorsten Greiner',
+    author_email='thorsten.greiner@posteo.de',
+    url='https://github.com/thgreiner/py-amy',
+    packages=find_packages(package_path),
+    package_dir={'': package_path},
+)
