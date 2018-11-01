065f0192ce4cd16d86ca3789c4206489aa3a411b
Thorsten Greiner <thorsten.greiner@zalando.de>
2018-11-01 16:53:05 +0100

    Introduce chess_input and use it in chess_pgn.

---
+++ chess_input.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+class Repr1:
+
+    def __init__(self):
+        self.SIZE_PER_COLOR = 48 + 5 * 64
+        self.SIZE = 2 * self.SIZE_PER_COLOR
+        self.eval_buf = np.ndarray((self.SIZE,))
+
+
+    def get_offset(self, square, piece_type):
+        if piece_type == 1:
+            return square - 8
+        else:
+            return 48 + (piece_type - 2) * 64 + square
+
+
+    def board_to_array(self, b):
+        self.eval_buf[:] = 0
+
+        if not b.turn:
+            b = b.mirror()
+
+        for piece_type in range(1, 7):
+            squares = b.pieces(piece_type, True)
+            for sq in squares:
+                self.eval_buf[self.get_offset(sq, piece_type)] = 1
+            squares = b.pieces(piece_type, False)
+            for sq in squares:
+                self.eval_buf[self.get_offset(sq, piece_type) + self.SIZE_PER_COLOR] = 1
+
+        return self.eval_buf
+
+class Repr2:
+
+    def __init__(self):
+        self.SIZE = 48 + 5 * 64 + 1
+        self.eval_buf = np.ndarray((self.SIZE,))
+
+
+    def get_offset(self, square, piece_type):
+        if piece_type == 1:
+            return square - 8
+        else:
+            return 48 + (piece_type - 2) * 64 + square
+
+
+    def board_to_array(self, b):
+        self.eval_buf[:] = 0
+
+        for piece_type in range(1, 7):
+            squares = b.pieces(piece_type, True)
+            for sq in squares:
+                self.eval_buf[self.get_offset(sq, piece_type)] = 1
+            squares = b.pieces(piece_type, False)
+            for sq in squares:
+                self.eval_buf[self.get_offset(sq, piece_type)] = -1
+        if b.turn:
+            self.eval_buf[self.SIZE - 1] = 1
+        else:
+            self.eval_buf[self.SIZE - 1] = -1
+
+        return self.eval_buf
--- chess_pgn.py
+++ chess_pgn.py
@@ -1,39 +1,65 @@
-from chess import Board, Move
+import chess
+from chess import Board, Move, Piece
 import random
 import numpy as np
 import chess.pgn
-from searcher import Searcher
+from searcher import Searcher, AmySearcher
+from chess_input import Repr1, Repr2
+import matplotlib.pyplot as plt
 
-POSITIONS_TO_LEARN_APRIORI = 300000
+def plot_history(history):
+    plt.figure()
+    plt.xlabel('Epoch')
+    plt.ylabel('Mean Abs Error')
+    plt.plot(history.epoch, np.array(history.history['loss']),
+             label='Train Loss')
+    plt.plot(history.epoch, np.array(history.history['mean_absolute_error']),
+             label = 'Val loss')
+    plt.legend()
+    plt.ylim([0, 1.5])
+    plt.show()
 
-SIZE_PER_COLOR = 49 + 5 * 65
-SIZE = 2 * SIZE_PER_COLOR + 3
+repr = Repr1()
 
-OPENING = 4
+# POSITIONS_TO_LEARN_APRIORI = 900000
+POSITIONS_TO_LEARN_APRIORI = 200
+
+OPENING = 1
 
 # TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras.models import load_model
 
 model1 = keras.Sequential([
-    keras.layers.Dense(200, input_shape=(SIZE, )),
+    keras.layers.Dense(200, input_shape=(repr.SIZE, )),
 #    keras.layers.Dropout(0.2),
     keras.layers.Dense(90),
 #    keras.layers.Dropout(0.1),
     keras.layers.Dense(1)
 ])
 
-model2 = keras.Sequential([
-    keras.layers.Dense(512, input_shape=(SIZE, )),
-    keras.layers.LeakyReLU(),
-    keras.layers.Dropout(0.2),
-    keras.layers.Dense(128),
-    keras.layers.LeakyReLU(),
-    keras.layers.Dropout(0.2),
-    keras.layers.Dense(32),
-    keras.layers.LeakyReLU(),
-    keras.layers.Dense(1)
-])
+if True:
+    model2 = keras.Sequential([
+        keras.layers.Dense(100, input_shape=(repr.SIZE, )),
+        keras.layers.LeakyReLU(),
+        keras.layers.Dropout(0.1),
+        keras.layers.Dense(100),
+        keras.layers.LeakyReLU(),
+        keras.layers.Dropout(0.1),
+        keras.layers.Dense(100),
+        keras.layers.LeakyReLU(),
+        keras.layers.Dropout(0.1),
+        keras.layers.Dense(100),
+        keras.layers.LeakyReLU(),
+        keras.layers.Dropout(0.1),
+        keras.layers.Dense(1)
+    ])
+else:
+    model2 = load_model("model2.h5")
+
+model2.summary()
+
 
 opt1 = tf.train.AdamOptimizer()
 opt2 = tf.train.RMSPropOptimizer(0.001)
@@ -46,7 +72,6 @@ model2.compile(optimizer=opt1,
                loss='mean_squared_error',
                metrics=['mae'])
 
-
 def label_for_result(result):
     if result == '1-0':
         return 1
@@ -56,62 +81,31 @@ def label_for_result(result):
 
 
 def phasing(label, moves_in_game, current_move):
-    return label / (1.0 + min(20, moves_in_game - current_move))
-
-
-def get_offset(square, piece_type):
-    if piece_type == 1:
-        return square - 8
-    else:
-        return 49 + (piece_type - 2) * 65 + square
-
-
-eval_buf = np.ndarray((SIZE,))
-
-
-def board_to_array(b):
-    global eval_buf
-    eval_buf[:] = 0
-
-    total_pieces = 0
-    for piece_type in range(1, 7):
-        balance = 0
-        squares = b.pieces(piece_type, True)
-        for sq in squares:
-            eval_buf[get_offset(sq, piece_type)] = 1
-            balance += 1
-            total_pieces += 1
-        squares = b.pieces(piece_type, False)
-        for sq in squares:
-            eval_buf[get_offset(sq, piece_type) + SIZE_PER_COLOR] = 1
-            balance -= 1
-            total_pieces += 1
-        eval_buf[get_offset(64, piece_type)] = balance
-    if b.turn:
-        eval_buf[SIZE - 2] = 1
-    else:
-        eval_buf[SIZE - 1] = 1
-    eval_buf[SIZE - 3] = total_pieces
-    return eval_buf
+    return 10 * label * (1.0 + moves_in_game - current_move) ** -0.8
 
 
 def evaluate(board, model):
-    input = board_to_array(board)
-    score = model.predict([input.reshape(1, SIZE)])[0][0]
-    if not b.turn:
-        score = -score
+    if b.turn:
+        input = repr.board_to_array(board)
+    else:
+        input = repr.board_to_array(board.mirror())
+
+    score = model.predict([input.reshape(1, repr.SIZE)])[0][0]
+    # score += random.uniform(-0.005, 0.005)
     return score
 
 
 pgn = open("ClassicGames.pgn")
 
 npos = POSITIONS_TO_LEARN_APRIORI
-train_data = np.zeros((npos, SIZE))
+train_data = np.zeros((npos, repr.SIZE))
 train_labels = np.zeros((npos))
 
 i = 0
 while True:
     game = chess.pgn.read_game(pgn)
+    if game is None:
+        break
     label = label_for_result(game.headers["Result"])
     b = game.board()
     nmoves = 0
@@ -119,11 +113,11 @@ while True:
     for move in game.main_line():
         is_capture = b.is_capture(move)
         if nmoves > OPENING and not is_capture:
-            train_data[i] = board_to_array(b)
-            train_labels[i] = phasing(label, moves_in_game, nmoves)
-            i += 1
-            train_data[i] = board_to_array(b.mirror())
-            train_labels[i] = -phasing(label, moves_in_game, nmoves)
+            train_data[i] = repr.board_to_array(b)
+            if b.turn:
+                train_labels[i] = phasing(label, moves_in_game, nmoves)
+            else:
+                train_labels[i] = -phasing(label, moves_in_game, nmoves)
             i += 1
         b.push(move)
         nmoves += 1
@@ -135,41 +129,59 @@ while True:
 
 print(i)
 
-searcher1 = Searcher(lambda board: evaluate(board, model1))
-searcher2 = Searcher(lambda board: evaluate(board, model2))
+npos = i
+train_data = np.resize(train_data, (npos, repr.SIZE))
+train_labels = np.resize(train_labels, (npos, ))
 
 offset = npos
+white_searcher = Searcher(lambda board: evaluate(board, model2))
+black_searcher = AmySearcher()
+
 while True:
 
-    model1.fit(train_data, train_labels, batch_size=128, epochs=3)
-    model2.fit(train_data, train_labels, batch_size=128, epochs=3)
+    # model1.fit(train_data, train_labels, batch_size=128, epochs=30)
+    history = model2.fit(train_data, train_labels, batch_size=1024, epochs=50)
+    print(history.history)
+    # plot_history(history)
+
+    model2.save("model2.h5")
+
+    # b = Board()
+    # start_pos = gen_kqk().fen()
+
+    white_searcher, black_searcher = black_searcher, white_searcher
 
     b = Board()
+    # b.set_fen(start_pos)
+
+    opening = [ Move.from_uci("d2d4"), Move.from_uci("d7d5") ]
     while not b.is_game_over():
         if len(b.move_stack) > OPENING:
             if b.turn:
-                move = searcher1.select_move(b)
+                move = white_searcher.select_move(b)
             else:
-                move = searcher2.select_move(b)
+                move = black_searcher.select_move(b)
         else:
-            move = random.choice(list(b.generate_legal_moves()))
+            # move = random.choice(list(b.generate_legal_moves()))
+            move = opening[len(b.move_stack)]
 
         print(b.san(move))
         b.push(move)
         print(b)
+        print(b.fen())
 
     print("{} after {} moves.".format(b.result(), b.fullmove_number))
 
     result = label_for_result(b.result())
     print(result)
 
-    n = 2 * (len(b.move_stack) - OPENING)
+    n = len(b.move_stack) - OPENING
 
     if offset == 0:
-        train_data = np.zeros((n, SIZE))
+        train_data = np.zeros((n, repr.SIZE))
         train_labels = np.zeros((n))
     else:
-        new_train_data = np.zeros((n + offset, SIZE))
+        new_train_data = np.zeros((n + offset, repr.SIZE))
         new_train_labels = np.zeros((n + offset))
         new_train_data[0:offset] = train_data
         new_train_labels[0:offset] = train_labels
@@ -180,12 +192,12 @@ while True:
     while len(b.move_stack) > OPENING:
         try:
             m = b.pop()
-            i = 2 * (len(b.move_stack) - OPENING)
-            train_data[offset + i] = board_to_array(b)
-            train_labels[offset + i] = phasing(result, moves_in_game, len(b.move_stack))
-            i += 1
-            train_data[offset + i] = board_to_array(b.mirror())
-            train_labels[offset + i] = -phasing(result, moves_in_game, len(b.move_stack))
+            i = (len(b.move_stack) - OPENING)
+            train_data[offset + i] = repr.board_to_array(b)
+            if b.turn:
+                train_labels[i] = phasing(label, moves_in_game, nmoves)
+            else:
+                train_labels[i] = -phasing(label, moves_in_game, nmoves)
         except:
             break
 
