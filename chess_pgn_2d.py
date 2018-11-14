79fde07fdf96eb25b9d2956f83f7011a2ae46a16
Thorsten Greiner <thorsten.greiner@consol.de>
2018-11-14 23:16:08 +0100

    Refactoring.

--- chess_pgn_2d.py
+++ chess_pgn_2d.py
@@ -1,21 +1,22 @@
+import sys
+import random
+
 import chess
 from chess import Board, Move, Piece
-import random
-import numpy as np
 import chess.pgn
-from searcher import Searcher, AmySearcher
+
 from chess_input import Repr2D
-import sys
 
-# POSITIONS_TO_LEARN_APRIORI = 900000
-POSITIONS_TO_LEARN_APRIORI = 4_000_000
-OPENING = 8
-MODEL_NAME='model-2d.h5'
-
-# TensorFlow and tf.keras
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import load_model
+
+# POSITIONS_TO_LEARN_APRIORI = 900000
+POSITIONS_TO_LEARN_APRIORI = 20_000
+OPENING = 8
+MODEL_NAME='model-2d.h5'
+
 
 def label_for_result(result):
     if result == '1-0':
@@ -29,58 +30,64 @@ def phasing(label, moves_in_game, current_move):
     # return label * (1.0 + moves_in_game - current_move) ** -0.8
     return label * (0.97 ** (moves_in_game - current_move))
 
-def train_model_from_pgn(file_name):
-    if False:
-        model2 = keras.Sequential([
-            keras.layers.Conv2D(96, (3, 3), input_shape=(8, 8, 12)),
-            keras.layers.BatchNormalization(axis = 3),
-            keras.layers.LeakyReLU(),
-            keras.layers.Conv2D(64, (3, 3)),
-            keras.layers.BatchNormalization(axis = 3),
-            keras.layers.LeakyReLU(),
-            keras.layers.Flatten(),
-            keras.layers.Dense(256),
-            keras.layers.LeakyReLU(),
-            keras.layers.Dense(1, activation='tanh')
-        ])
-    else:
-        model2 = load_model(MODEL_NAME)
 
-    model2.summary()
+def create_model():
+    board_input = keras.layers.Input(shape = (8, 8, 12), name='board_input')
+    castle_input =  keras.layers.Input(shape = (4,), name='castle_input')
+    
+    conv1 = keras.layers.Conv2D(96, (3, 3), name='conv1')(board_input)
+    norm1 = keras.layers.BatchNormalization(axis = 3, name='norm1')(conv1)
+    leaky1 = keras.layers.LeakyReLU(0.01, name='leaky1')(norm1)
+    
+    conv2 = keras.layers.Conv2D(64, (3, 3), name='conv2')(leaky1)
+    norm2 = keras.layers.BatchNormalization(axis = 3, name='norm2')(conv2)
+    leaky2 = keras.layers.LeakyReLU(0.01, name='leaky2')(norm2)
+    
+    flatten = keras.layers.Flatten(name='flatten')(leaky2)
+    concat = keras.layers.concatenate(inputs=[flatten, castle_input], name='concat')
+    
+    dense1 = keras.layers.Dense(256, name='dense1')(concat)
+    leaky3 = keras.layers.LeakyReLU(0.01, name='leaky3')(dense1)
+
+    output = keras.layers.Dense(1, activation='tanh', name='output')(leaky3)
+
+    return keras.Model(inputs = (board_input, castle_input), outputs=output)
 
 
-    opt1 = keras.optimizers.Adadelta()
-
-    model2.compile(optimizer=opt1,
-                   loss='mean_squared_error',
-                   metrics=['mae'])
-
+def parse_pgn_to_training_data(file_name):
     pgn = open(file_name)
 
     npos = POSITIONS_TO_LEARN_APRIORI
     repr = Repr2D()
-    train_data = np.zeros((npos, 8, 8, 12), np.int8)
+    train_data_pos = np.zeros((npos, 8, 8, 12), np.int8)
+    train_data_castle = np.zeros((npos, 4), np.int8)
     train_labels = np.zeros((npos), np.float32)
 
     i = 0
     ngames = 0
     while True:
+
         try:
             game = chess.pgn.read_game(pgn)
         except UnicodeDecodeError or ValueError:
-            pass
+            continue
+
         if game is None:
             break
+
         label = label_for_result(game.headers["Result"])
         if label == 0:
             continue
+
         b = game.board()
         nmoves = 0
         moves_in_game = len(list(game.main_line()))
+
         for move in game.main_line():
             is_capture = b.is_capture(move)
-            if nmoves > OPENING and not is_capture:
-                train_data[i] = repr.board_to_array(b)
+            if nmoves >= OPENING and not is_capture:
+                train_data_pos[i] = repr.board_to_array(b)
+                train_data_castle[i] = repr.castling_to_array(b)
                 if b.turn:
                     train_labels[i] = phasing(label, moves_in_game, nmoves)
                 else:
@@ -90,20 +97,43 @@ def train_model_from_pgn(file_name):
             nmoves += 1
             if i >= npos:
                 break
+
         if i >= npos:
             break
+
         ngames += 1
         print("Games: {}, Positions: {}".format(ngames, i), end='\r')
 
     print("Games: {}, Positions: {}".format(ngames, i))
 
-    npos = i
-    train_data = train_data[:npos]
-    train_labels = train_labels[:npos]
+    train_data_pos = train_data_pos[:i]
+    train_data_castle = train_data_castle[:i]
+    train_labels = train_labels[:i]
+    
+    return ([train_data_pos, train_data_castle], train_labels)
+
+
+def train_model_from_pgn(file_name):
+
+    if False:
+        model = create_model()
+    else:
+        model = load_model(MODEL_NAME)
+
+    model.summary()
+
+    optimizer = keras.optimizers.Adadelta()
+
+    model.compile(optimizer=optimizer,
+                  loss='mean_squared_error',
+                  metrics=['mae'])
+
+    train_data, train_labels = parse_pgn_to_training_data(file_name)
 
     while True:
-        history = model2.fit(train_data, train_labels, batch_size=128, epochs=3)
-        model2.save(MODEL_NAME)
+        history = model.fit(train_data, train_labels, batch_size=128, epochs=3,
+                             validation_split = 0.1)
+        model.save(MODEL_NAME)
 
 
 if __name__ == '__main__':
