a229e5361eb65357140ccfba008e88d12a169ecc
Thorsten Greiner <thorsten.greiner@consol.de>
2018-12-18 22:52:51 +0100

    Updated move policy training.

--- chess_pgn_move.py
+++ chess_pgn_move.py
@@ -27,43 +27,80 @@ def label_for_result(result):
 
 def add_common_layers(y):
     y = keras.layers.BatchNormalization()(y)
-    y = keras.layers.LeakyReLU(LEAK)(y)
+    y = keras.layers.LeakyReLU()(y)
     return y
 
+cardinality = 4
 
-def residual_block(y):
+def grouped_convolution(y, nb_channels = 96, _strides = (1,1)):
+    # when `cardinality` == 1 this is just a standard convolution
+    if cardinality == 1:
+        return keras.layers.Conv2D(nb_channels, kernel_size=(3, 3), strides=_strides, padding='same')(y)
+    
+    assert not nb_channels % cardinality
+    _d = nb_channels // cardinality
+
+    # in a grouped convolution layer, input and output channels are divided into `cardinality` groups,
+    # and convolutions are separately performed within each group
+    groups = []
+    for j in range(cardinality):
+        group = keras.layers.Lambda(lambda z: z[:, :, :, j * _d:j * _d + _d], output_shape=(8, 8, 24))(y)
+        groups.append(keras.layers.Conv2D(_d, kernel_size=(3, 3), strides=_strides, padding='same')(group))
+        
+    # the grouped convolutional layer concatenates them as the outputs of the layer
+    y = keras.layers.concatenate(groups)
+
+    return y
+
+def residual_block(y, nb_channels_in=96, nb_channels_out=96, _strides=(1, 1), _project_shortcut=False):
+    """
+    Our network consists of a stack of residual blocks. These blocks have the same topology,
+    and are subject to two simple rules:
+    - If producing spatial maps of the same size, the blocks share the same hyper-parameters (width and filter sizes).
+    - Each time the spatial map is down-sampled by a factor of 2, the width of the blocks is multiplied by a factor of 2.
+    """
     shortcut = y
 
-    y = keras.layers.Conv2D(64, (1, 1), padding='same')(y)
+    # we modify the residual building block as a bottleneck design to make the network more economical
+    y = keras.layers.Conv2D(nb_channels_in, kernel_size=(1, 1), strides=(1, 1), padding='same')(y)
     y = add_common_layers(y)
 
-    y = keras.layers.Conv2D(64, (3, 3), padding='same')(y)
+    # ResNeXt (identical to ResNet when `cardinality` == 1)
+    y = grouped_convolution(y, nb_channels_in, _strides=_strides)
     y = add_common_layers(y)
 
-    y = keras.layers.Conv2D(64, (1, 1), padding='same')(y)
+    y = keras.layers.Conv2D(nb_channels_out, kernel_size=(1, 1), strides=(1, 1), padding='same')(y)
+    # batch normalization is employed after aggregating the transformations and before adding to the shortcut
     y = keras.layers.BatchNormalization()(y)
-    y = keras.layers.add([shortcut, y])
-    y = keras.layers.LeakyReLU(LEAK)(y)
-    return y
 
+    # identity shortcuts used directly when the input and output are of the same dimensions
+    if _project_shortcut or _strides != (1, 1):
+        # when the dimensions increase projection shortcut is used to match dimensions (done by 1×1 convolutions)
+        # when the shortcuts go across feature maps of two sizes, they are performed with a stride of 2
+        shortcut = keras.layers.Conv2D(nb_channels_out, kernel_size=(1, 1), strides=_strides, padding='same')(shortcut)
+        shortcut = keras.layers.BatchNormalization()(shortcut)
+
+    y = keras.layers.add([shortcut, y])
+
+    # relu is performed right after each batch normalization,
+    # expect for the output of the block where relu is performed after the adding to the shortcut
+    y = keras.layers.LeakyReLU()(y)
+
+    return y
 
 
 def create_model():
     board_input = keras.layers.Input(shape = (8, 8, 12), name='board_input')
 
-    conv1 = keras.layers.Conv2D(64, (3, 3), name='conv1', padding='same')(board_input)
-    norm1 = keras.layers.BatchNormalization(axis = 3, name='norm1')(conv1)
-    leaky1 = keras.layers.LeakyReLU(LEAK, name='leaky1')(norm1)
+    conv1 = keras.layers.Conv2D(96, (3, 3), name='conv1', padding='same')(board_input)
+    temp = add_common_layers(conv1)
 
-    temp = residual_block(leaky1)
     temp = residual_block(temp)
     temp = residual_block(temp)
     temp = residual_block(temp)
+    temp = residual_block(temp)
 
-    temp = keras.layers.Conv2D(1, (1, 1), name='conv1x1', padding='same')(temp)
-    temp = add_common_layers(temp)
-
-    output = keras.layers.Conv2D(7, (1, 1), activation='relu', name='output')(temp)
+    output = keras.layers.Conv2D(7, (3, 3), activation='relu', padding='same', name='output')(temp)
 
     return keras.Model(inputs = board_input, outputs=output)
 
@@ -126,6 +163,10 @@ if cnt1 < npos:
     train_data = train_data[:cnt1]
     train_labels = train_labels[:cnt1]
 
+save_callback = keras.callbacks.ModelCheckpoint("weights-move-{epoch:02d}-{val_loss:.2f}.hdf5")
+
 while True:
-    history = model.fit(train_data, train_labels, batch_size=1024, epochs=5)
+    history = model.fit(train_data, train_labels, batch_size=256, epochs=25,
+        validation_split=0.1,
+        callbacks=[save_callback])
     model.save("move-model.h5")
