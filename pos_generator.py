4db9d69c7317bc7705b0d167c3aba5f598016b3c
Thorsten Greiner <thorsten.greiner@zalando.de>
2019-01-02 16:39:19 +0100

    Generate KQK for white/black queens.

--- pos_generator.py
+++ pos_generator.py
@@ -8,7 +8,7 @@ def generate_kqk():
         board.clear()
         positions = random.sample(range(64), 3)
         board.set_piece_at(positions[0], Piece.from_symbol("K"))
-        board.set_piece_at(positions[1], Piece.from_symbol("Q"))
+        board.set_piece_at(positions[1], Piece.from_symbol(random.choice(['Q', 'q'])))
         board.set_piece_at(positions[2], Piece.from_symbol("k"))
         is_ok = board.is_valid()
     return board
