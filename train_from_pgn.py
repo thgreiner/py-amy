2dc74ff8681d54885a43bd1d4b174fd2709bc2ca
Thorsten Greiner <thorsten.greiner@consol.de>
2019-01-03 22:21:10 +0100

    Regularization throughout. More intermediate layers.

--- train_from_pgn.py
+++ train_from_pgn.py
@@ -47,9 +47,15 @@ def my_categorical_crossentropy(y_true, y_pred):
 
 def residual_block(y, dim):
     shortcut = y
-    y = keras.layers.Conv2D(3 * dim, (1, 1), padding='same', activation='elu', kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(y)
-    y = keras.layers.DepthwiseConv2D((3, 3), padding='same', activation='elu', kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(y)
-    y = keras.layers.Conv2D(dim, (1, 1), padding='same', activation='elu', kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(y)
+    y = keras.layers.Conv2D(3 * dim, (1, 1), padding='same',
+                                             activation='elu',
+                                             kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(y)
+    y = keras.layers.DepthwiseConv2D((3, 3), padding='same',
+                                             activation='elu',
+                                             kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(y)
+    y = keras.layers.Conv2D(dim, (1, 1), padding='same',
+                                         activation='elu',
+                                         kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(y)
     y = keras.layers.add([y, shortcut])
     return y
 
@@ -58,7 +64,7 @@ def create_model():
     board_input = keras.layers.Input(shape = (8, 8, 17), name='board_input')
     moves_input = keras.layers.Input(shape = (4672,), name='moves_input')
 
-    dim = 64
+    dim = 67
 
     temp = keras.layers.Conv2D(dim, (3, 3), padding='same',
                                             activation='elu',
@@ -67,17 +73,26 @@ def create_model():
     for i in range(13):
         temp = residual_block(temp, dim)
 
-    t2 = keras.layers.Conv2D(dim, (3, 3), padding='same', activation='elu', kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(temp)
-    t2 = keras.layers.Conv2D(73, (3, 3), activation='linear', padding='same')(t2)
+    t2 = keras.layers.Conv2D(dim, (3, 3), padding='same',
+                                          activation='elu',
+                                          kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(temp)
+    t2 = keras.layers.Conv2D(73, (3, 3), activation='linear',
+                                         padding='same',
+                                         kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(t2)
     t2 = keras.layers.Flatten()(t2)
     t2 = keras.layers.multiply([t2, moves_input])
     move_output = keras.layers.Activation("softmax", name='moves')(t2)
 
-    temp = keras.layers.Conv2D(1, (1, 1), padding='same', activation='elu')(temp)
+    temp = keras.layers.Conv2D(1, (1, 1), padding='same',
+                                          activation='elu',
+                                          kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(temp)
     temp = keras.layers.Flatten()(temp)
-    temp = keras.layers.Dense(256, activation='elu', kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(temp)
+    temp = keras.layers.Dense(256, activation='elu',
+                                   kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(temp)
 
-    score_output = keras.layers.Dense(1, activation='tanh', name='score')(temp)
+    score_output = keras.layers.Dense(1, activation='tanh',
+                                         kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT),
+                                         name='score')(temp)
 
     return keras.Model(inputs = [board_input, moves_input], outputs=[move_output, score_output])
 
