f0b9a5879db7e47de2644ddee0a53fb9667accbb
Thorsten Greiner <thorsten.greiner@consol.de>
2019-01-06 15:53:34 +0100

    Moved neural network code to network.py.

---
+++ network.py
@@ -0,0 +1,80 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import load_model
+from tensorflow.keras import backend as K
+
+REGULARIZATION_WEIGHT=1e-5
+
+def residual_block(y, dim):
+    shortcut = y
+    y = keras.layers.Conv2D(3 * dim, (1, 1), padding='same',
+                                             activation='elu',
+                                             kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(y)
+    y = keras.layers.DepthwiseConv2D((3, 3), padding='same',
+                                             activation='elu',
+                                             kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(y)
+    y = keras.layers.Conv2D(dim, (1, 1), padding='same',
+                                         activation='elu',
+                                         kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(y)
+    y = keras.layers.add([y, shortcut])
+    return y
+
+
+def create_model():
+    board_input = keras.layers.Input(shape = (8, 8, repr.num_planes), name='board_input')
+    moves_input = keras.layers.Input(shape = (4672,), name='moves_input')
+
+    dim = 48
+
+    temp = keras.layers.Conv2D(128, (3, 3), padding='same',
+                                            activation='elu',
+                                            kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(board_input)
+
+    temp = keras.layers.Conv2D(dim, (1, 1), padding='same',
+                                            activation='elu',
+                                            kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(temp)
+    for i in range(15):
+        temp = residual_block(temp, dim)
+
+    t2 = keras.layers.Conv2D(73, (3, 3), padding='same',
+                                         activation='elu',
+                                         kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(temp)
+
+    t2 = keras.layers.Conv2D(73, (3, 3), activation='linear',
+                                         padding='same',
+                                         kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(t2)
+    t2 = keras.layers.Flatten()(t2)
+    t2 = keras.layers.multiply([t2, moves_input])
+    move_output = keras.layers.Activation("softmax", name='moves')(t2)
+
+    temp = keras.layers.Conv2D(8, (1, 1), padding='same',
+                                          activation='elu',
+                                          kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(temp)
+    temp = keras.layers.Flatten()(temp)
+    temp = keras.layers.Dense(192, activation='elu',
+                                   kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(temp)
+
+    score_output = keras.layers.Dense(1, activation='tanh',
+                                         kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT),
+                                         name='score')(temp)
+
+    return keras.Model(inputs = [board_input, moves_input], outputs=[move_output, score_output])
+
+
+def load_or_create_model(model_name):
+    if model_name is None:
+        model = create_model()
+    else:
+        print("Loading model from \"{}\"".format(model_name))
+        model = load_model(model_name)
+
+    model.summary()
+
+    optimizer = keras.optimizers.Adam()
+    # optimizer = keras.optimizers.SGD(lr=0.2, momentum=0.9)
+
+    model.compile(optimizer=optimizer,
+                  loss={'moves': 'categorical_crossentropy', 'score': 'mean_squared_error' },
+                  metrics=['accuracy', 'mae'])
+    return model
--- train_from_pgn.py
+++ train_from_pgn.py
@@ -3,23 +3,18 @@ from chess import Board
 import numpy as np
 import chess.pgn
 from chess_input import Repr2D
-# TensorFlow and tf.keras
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.models import load_model
-from tensorflow.keras import backend as K
 
 import sys
 import time
 
+from network import load_or_create_model
+
 # Training batch size
-BATCH_SIZE = 128
+BATCH_SIZE = 256
 
 # Checkpoint every x batches
-CHECKPOINT = 400
+CHECKPOINT = 200
 
-
-REGULARIZATION_WEIGHT=2e-5
 
 def label_for_result(result, turn):
     if result == '1-0':
@@ -36,88 +31,21 @@ def label_for_result(result, turn):
     return 0
 
 
-def phasing(label, moves_in_game, current_move):
-    # return label * (1.0 + moves_in_game - current_move) ** -0.8
-    return label # * (0.99 ** (moves_in_game - current_move))
-
-
-def residual_block(y, dim):
-    shortcut = y
-    y = keras.layers.Conv2D(3 * dim, (1, 1), padding='same',
-                                             activation='elu',
-                                             kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(y)
-    y = keras.layers.DepthwiseConv2D((3, 3), padding='same',
-                                             activation='elu',
-                                             kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(y)
-    y = keras.layers.Conv2D(dim, (1, 1), padding='same',
-                                         activation='elu',
-                                         kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(y)
-    y = keras.layers.add([y, shortcut])
-    return y
-
-
-def create_model():
-    board_input = keras.layers.Input(shape = (8, 8, 16), name='board_input')
-    moves_input = keras.layers.Input(shape = (4672,), name='moves_input')
-
-    dim = 67
-
-    temp = keras.layers.Conv2D(dim, (3, 3), padding='same',
-                                            activation='elu',
-                                            kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(board_input)
-
-    for i in range(13):
-        temp = residual_block(temp, dim)
-
-    t2 = keras.layers.Conv2D(73, (3, 3), padding='same',
-                                         activation='elu',
-                                         kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(temp)
-
-    t2 = keras.layers.Conv2D(73, (3, 3), activation='linear',
-                                         padding='same',
-                                         kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(t2)
-    t2 = keras.layers.Flatten()(t2)
-    t2 = keras.layers.multiply([t2, moves_input])
-    move_output = keras.layers.Activation("softmax", name='moves')(t2)
-
-    temp = keras.layers.Conv2D(1, (1, 1), padding='same',
-                                          activation='elu',
-                                          kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(temp)
-    temp = keras.layers.Flatten()(temp)
-    temp = keras.layers.Dense(256, activation='elu',
-                                   kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT))(temp)
-
-    score_output = keras.layers.Dense(1, activation='tanh',
-                                         kernel_regularizer=keras.regularizers.l2(REGULARIZATION_WEIGHT),
-                                         name='score')(temp)
-
-    return keras.Model(inputs = [board_input, moves_input], outputs=[move_output, score_output])
-
-def load_or_create_model(model_name):
-    if model_name is None:
-        model = create_model()
-    else:
-        print("Loading model from \"{}\"".format(model_name))
-        model = load_model(model_name)
-
-    model.summary()
-
-    optimizer = keras.optimizers.Adam()
-    # optimizer = keras.optimizers.SGD(lr=0.2, momentum=0.9)
-
-    model.compile(optimizer=optimizer,
-                  loss={'moves': 'categorical_crossentropy', 'score': 'mean_squared_error' },
-                  metrics=['accuracy', 'mae'])
-    return model
-
 def stats(step_output):
     loss = step_output[0]
+    moves_loss = step_output[1]
+    score_loss = step_output[2]
+    reg_loss = loss - moves_loss - score_loss
+
     moves_accuracy = step_output[3]
     score_mae = step_output[6]
 
-    return "loss: {:.3f}, move accuracy: {:.0f}%, score mae: {:.3f}".format(
-        loss, moves_accuracy * 100, score_mae
+    return "loss: {:.2f} = {:.2f} + {:.2f} + {:.2f}, move accuracy: {:2.0f}%, score mae: {:.2f}".format(
+        loss, 
+        moves_loss, score_loss, reg_loss,
+        moves_accuracy * 100, score_mae
     )
+
 
 if __name__ == "__main__":
     repr = Repr2D()
@@ -128,10 +56,12 @@ if __name__ == "__main__":
 
     model = load_or_create_model(model_name)
 
-    train_data_board = np.zeros(((BATCH_SIZE, 8, 8, 16)), np.int8)
+    train_data_board = np.zeros(((BATCH_SIZE, 8, 8, repr.num_planes)), np.int8)
     train_data_moves = np.zeros((BATCH_SIZE, 4672), np.int8)
     train_labels1 = np.zeros((BATCH_SIZE, 4672), np.int8)
     train_labels2 = np.zeros((BATCH_SIZE, 1), np.float32)
+
+    start_time = time.perf_counter()
 
     for iteration in range(100):
         with open(sys.argv[1]) as pgn:
@@ -177,13 +107,13 @@ if __name__ == "__main__":
                             train_data = [ train_data_board, train_data_moves]
                             train_labels = [ train_labels1, train_labels2 ]
 
-                            start_time = time.perf_counter()
                             results = model.train_on_batch(train_data, train_labels)
                             elapsed = time.perf_counter() - start_time
 
                             samples += cnt
                             print("{}.{}: {} in {:.1f}s [{} games]".format(
                                 iteration, samples, stats(results), elapsed, ngames))
+                            start_time = time.perf_counter()
 
                             cnt = 0
                             if samples >= checkpoint_next:
