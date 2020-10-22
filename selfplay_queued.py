6b49dc33d749ddb6245be553d4c05bb71bc5d40d
Thorsten Greiner <thorsten.greiner@posteo.de>
2020-10-22 08:16:43 +0200

    Add argparse so model can be supplied.

--- selfplay_queued.py
+++ selfplay_queued.py
@@ -4,12 +4,15 @@ from selfplay import selfplay
 from queued_evaluator import QueuedEvaluator, MultiplexingEvaluator
 from threading import Thread
 from queue import Queue
+import argparse
 
 if __name__ == "__main__":
 
-    model_name = "combined-model.h5"
+    parser = argparse.ArgumentParser(description="Execute parallel selfplay.")
+    parser.add_argument('--model', help="model file name")
+    args = parser.parse_args()
 
-    multiplexing_evaluator = MultiplexingEvaluator(model_name, 20)
+    multiplexing_evaluator = MultiplexingEvaluator(args.model, 20)
     input_queue = Queue()
 
     eval_thread = Thread(target = lambda: multiplexing_evaluator.run(input_queue))
